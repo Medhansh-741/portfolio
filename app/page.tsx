b9ee07b58482ab4ec25062455161a9cbdbb396b0
Medhansh-741 <medhansh541@gmail.com>
2026-08-02 17:24:29 +0530

    feat: add mobile-first layout shell below xl breakpoint

--- app/page.tsx
+++ app/page.tsx
@@ -5,20 +5,24 @@ import LeetCodeWidget from "./components/LeetCodeWidget";
 import CodeforcesWidget from "./components/CodeforcesWidget";
 import CommitFeed from "./components/CommitFeed";
 import GithubStatsWidget from "./components/GithubStatsWidget";
+import MobileHome from "./components/mobile/MobileHome";
 
 export default function Home() {
   return (
-    <main className="flex-1 flex flex-col bg-background overflow-visible xl:[@media(min-height:768px)]:overflow-hidden">
-      <GridHero
-        githubCalendar={<GithubCalendar />}
-        leetCodeWidget={<LeetCodeWidget delay={0.55} />}
-        codeforcesWidget={<CodeforcesWidget delay={0.65} />}
-        commitFeed={<CommitFeed delay={0.7} />}
-        githubStatsWidget={<GithubStatsWidget delay={0.75} />}
-      />
-      <div className="hidden xl:block mt-auto">
-        <SkillStrip />
-      </div>
-    </main>
+    <>
+      <main className="hidden xl:flex flex-1 flex-col bg-background overflow-visible xl:[@media(min-height:768px)]:overflow-hidden">
+        <GridHero
+          githubCalendar={<GithubCalendar />}
+          leetCodeWidget={<LeetCodeWidget delay={0.55} />}
+          codeforcesWidget={<CodeforcesWidget delay={0.65} />}
+          commitFeed={<CommitFeed delay={0.7} />}
+          githubStatsWidget={<GithubStatsWidget delay={0.75} />}
+        />
+        <div className="hidden xl:block mt-auto">
+          <SkillStrip />
+        </div>
+      </main>
+      <MobileHome />
+    </>
   );
 }
